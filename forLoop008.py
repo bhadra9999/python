313b87fb46be9b20b6a53f605e8a37615c9b2e35
bhadra9999 <raj.devops9999@gmail.com>
2018-11-06 16:14:51 +0530

    this is forLoop program

---
+++ forLoop008.py
@@ -0,0 +1,15 @@
+
+for letter in "Linux Academy":
+    print(letter)
+
+friends = ["linux", "devops", "aws"]
+
+for friend in friends:
+    print(friend)
+
+
+for index in range(10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
